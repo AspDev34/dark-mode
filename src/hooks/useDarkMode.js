7ab38fb75c0f490585f85a09aac87c242696f916
Ike Otu <ikeotu@msn.com>
2020-02-13 08:44:11 -0800

    useDarkMode created

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,12 @@
+import useLocalStorage from './useLocalStorage';
+import { useEffect } from 'react';
+
+const useDarkMode = () => {
+    const [someValue, setSomeValue] = useLocalStorage('darkMode');
+
+    useEffect (() => {
+        someValue ? document.querySelector('body').classList.add('dark-mode') : document.querySelector('body').classList.remove('dark-mode')
+    }, [someValue])
+}
+
+export default useDarkMode;
